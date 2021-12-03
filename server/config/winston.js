f440db7567df5eeb2bf386d4fc5b22c9fca56316
CesarRomeroRuiz <cesar25.rui96@gmail.com>
2021-12-02 20:19:35 -0600

    Instalando Winston (Cesar)

--- server/config/winston.js
+++ server/config/winston.js
@@ -1,7 +1,10 @@
 // Importando a winston
 import winston, { format } from 'winston';
+import appRoot from 'app-root-path';
+
 // Componentes para crear el formato
-const { combine, timestamp, label, printf } = format;
+const { combine, timestamp, printf, uncolorize, json, colorize } = format;
+
 // Perfil color para log
 const colors = {
   error: 'red',
@@ -15,15 +18,68 @@ const colors = {
 winston.addColors(colors);
 
 // Formato de consola
-const myFormat = format.combine(
-  format.colorize({ all: true }),
-  format.timestamp(),
-  format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`),
+const myFormat = combine(
+  colorize({ all: true }),
+  timestamp(),
+  printf((info) => `${info.timestamp} ${info.level}: ${info.message}`),
 );
 
 // Formato salida archivos log
-const myFileFormat = format.combine(
-  format.colorize(),
-  format.timestamp(),
-  format.jston(),
+const myFileFormat = combine(
+  uncolorize(),
+  timestamp(),
+  json(),
 );
+
+// Creando objetos de configuracion
+const options = {
+  infoFile: {
+    level: 'info',
+    filename: `${appRoot}/server/logs/infos.log`,
+    handleExceptions: true,
+    maxsize: 5242880, // 5MB
+    maxFiles: 5,
+    format: myFileFormat,
+  },
+  warnFile: {
+    level: 'warn',
+    filename: `${appRoot}/server/logs/warns.log`,
+    handleExceptions: true,
+    maxsize: 5242880, // 5MB
+    maxFiles: 5,
+    format: myFileFormat,
+  },
+  errorFile: {
+    level: 'error',
+    filename: `${appRoot}/server/logs/errors.log`,
+    handleExceptions: true,
+    maxsize: 5242880, // 5MB
+    maxFiles: 5,
+    format: myFileFormat,
+  },
+  console: {
+    level: 'debug',
+    handleExceptions: true,
+    format: myFormat,
+  }
+};
+
+// Creando la instancia del logger
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.File(options,infoFile),
+    new winston.transports.File(options,warnFile),
+    new winston.transports.File(options,errorFile),
+    new winston.transports.Console(options,console),
+  ],
+  exitOnError: false, // No finaliza en excepciones manejadas
+});
+
+// Manejo de un stream de entrada
+logger.stream = {
+  write(message) {
+    logger.info(message);
+  },
+};
+
+export default logger;
